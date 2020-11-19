60fe48a080c1d195727a8cf93b6114c035b4cb91
刘阳 <249606097@qq.com>
2020-11-19 20:25:17 +0800

    feat: 响应处理函数

---
+++ apps/utils/response_processor.py
@@ -0,0 +1,43 @@
+import json
+
+from django.http import HttpResponse
+
+from apps.utils.response_status import ResponseStatus
+
+
+def process_response(request) -> HttpResponse:
+    """
+    根据请求 request 中的状态 status 和数据 data 生成 JSON 格式的响应内容,
+    包装成 HttpResponse 对象
+
+    其中如果 status 不存在或类型错误, 则以意外错误作为响应状态
+
+    响应内容格式一般如下:
+    {
+        "code": 20000,
+        "msg": "成功",
+        "data": {...}
+    }
+
+    :param request: Request 对象
+    :return: 响应 HttpResponse 对象
+    """
+    # 响应内容
+    content = {}
+
+    # 响应状态处理
+    if not hasattr(request, 'status') or not isinstance(request.status, ResponseStatus):
+        request.status = ResponseStatus.UNEXPECTED_ERROR
+    content['code'] = request.status.value[0]
+    content['msg'] = request.status.value[1]
+
+    # 响应数据处理
+    if hasattr(request, 'data'):
+        content['data'] = request.data
+
+    # 响应内容 JSON 格式化
+    content = json.dumps(content)
+    return HttpResponse(content,
+                        content_type='application/json',
+                        status='200',
+                        charset='utf-8')

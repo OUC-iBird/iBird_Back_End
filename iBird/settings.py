6a5f89dc0fd2412c57bc1bc0b13b890528917e6f
刘阳 <249606097@qq.com>
2020-11-26 16:54:07 +0800

    feat: 安全的配置方式

--- iBird/settings.py
+++ iBird/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import json
+
+# Configuration File
+with open('./conf.json', 'r') as f:
+    config = json.load(f)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cfed4b-wyac-ru=@!50o3%nqo!u)c!e9ne^vm)b4ns0s0xm_vo'
+SECRET_KEY = config.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
